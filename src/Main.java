6deae6d6dcbd920b6791b1084ac58e8dc3d19b94
plzprayme <dbfpzk142@gmail.com>
2020-11-12 23:12:32 +0900

    세탁기 완료

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 세탁기 설치
+        WashingMachine wm = new WashingMachine();
+
+        // 무한반복
+        while (true) {
+            System.out.println(">> 환영합니다! 무엇을 도와드릴까요?");
+            System.out.println(">> 1. 세탁기 사용하기 2. 건조기 사용하기 3. 게임기 사용하기 4. 나가기");
+            System.out.print("<< 번호를 입력하세요: ");
+            int task = new Scanner(System.in).nextInt();
+
+            switch (task) {
+                case 1:
+                    // 돈을 넣은 후 세탁기 실행 가능한지 체크
+                    if (wm.insertCoin(4000)) {
+                        wm.run();
+                    }
+
+                    break;
+                case 2:
+                    // 건조기 사용
+                    break;
+                case 3:
+                    // 게임기 사용
+                    break;
+                case 4:
+                    System.out.println(">> 안녕히 가세요!");
+                    return;
+                default:
+                    System.out.println(">> 존재하지 않는 선택지입니다!");
+                    break;
+            }
+
+        }
+
+    }
+}
---
+++ src/WashingMachine.java
@@ -0,0 +1,86 @@
+public class WashingMachine {
+
+    int cost;
+    int seje;
+    long startTime;
+    boolean isRunning;
+
+    // final이 붙으면 다시는 수정할 수 없다.
+    // 자바에서 상수를 선언할 때는 final을 사용
+    final static int PRICE = 4000;
+
+    public WashingMachine() {
+        this.cost = 0;
+        this.seje = 2;
+        isRunning = false;
+    }
+
+    public void run() {
+        // 세탁기가 이미 동작하고 있기 때문에 동작할 수 없다.
+        // 세탁기가 동작한지 10분이 넘었으면 세탁기를 종료한다.
+        if (isRunning) {
+            // 시간 비교를 위해 현재시간 가져오기
+            long currentTime = System.currentTimeMillis();
+            // 작동한 시간 얻기
+            long runningTime = (currentTime - startTime) / 1000;
+            // 10초 이상 작동헀는지 체크하기
+            boolean isDone = runningTime >= 10;
+            // 10초 이상 작동 안했으면 아래 코드가 실행되지 않도록 종료
+            if (!isDone) {
+                System.out.println(">> 세탁기가 이미 작동 중입니다! " + (10 - runningTime) + "초 남았습니다!");
+                return;
+            }
+
+            // 10초 이상 작동 했으면 작동중이 아니도록 변수를 수정
+            isRunning = !isRunning;
+        }
+
+
+        // 세탁기가 동작하고 있기 때문에 동작할 수 있다.
+        if (!isRunning) {
+            // 세제하나 차감
+            this.seje -= 1;
+            // 돈 초기화
+            this.cost = 0;
+            // 실행중인 것 기록
+            isRunning = !isRunning;
+            // 시간 기록
+            startTime = System.currentTimeMillis();
+
+            System.out.println(">> 세탁을 시작합니다. 10초가 소요됩니다.");
+        }
+
+        System.out.println();
+    }
+
+    public boolean insertCoin(int cost) {
+        // 파라미터로 받은 cost를 보유하고 있는 this.cost에 더한다.
+        this.cost += cost;
+
+
+        // 돈도 충분하고 세제도 충분할 때만 true 반환
+        return isEnoughCoin() && isEnoughSeje();
+    }
+
+    // 돈을 충분히 받았는가?
+    private boolean isEnoughCoin() {
+        boolean isEnough = this.cost >= PRICE;
+        if (!isEnough) {
+            System.out.println("돈을 더 넣으세요!");
+        }
+
+        return isEnough;
+    }
+
+    // 보유 세제가 0보다 많은가?
+    private boolean isEnoughSeje() {
+        boolean isEnough = this.seje > 0;
+        if (!isEnough) {
+            System.out.println("세제가 부족합니다!!");
+        }
+
+        return isEnough;
+    }
+
+
+}
